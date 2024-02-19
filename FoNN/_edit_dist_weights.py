6d3953b953934ca121a8b3a5867cd00e6dbf19fb
danDiamo <78231894+danDiamo@users.noreply.github.com>
2024-02-19 16:33:43 +0000

    Update edit_dist_weights.py -- remove unused weightings

--- FoNN/_edit_dist_weights.py
+++ FoNN/_edit_dist_weights.py
@@ -12,16 +12,10 @@ index = [chr(i) for i in range(128)]
 insertion = np.ones(128, dtype=np.float64)
 insertion_ham = deletion_ham = insertion + 1
 
-# New: weight insertion by consonance
 scale_degrees = (str(i) for i in range(1, 8))
 scale_degrees_ascii = [ord(i) for i in scale_degrees]
-# 'harsh substitution scoring:
-# insertion[scale_degrees_ascii] = [0, 2, 0.5, 1, 0.5, 1, 2]
-# 'mid' scoring:
-# insertion_lev[scale_degrees_ascii] = [0, 1, 0.5, 1, 0.5, 1, 1]
-
-# NOTE: scoring above is impractical on full The Session corpus, allowing too many matches.
-# Try the following alternative for custom-weighted Levenshtein on large corpora
+# experimental custom-weighted Levenshtein insertion scoring
+# [not currently applied in any of the similarity search methodologies]
 insertion[scale_degrees_ascii] = [1, 2, 1, 1, 1, 1, 2]
 deletion_lev = insertion_lev = insertion
 
